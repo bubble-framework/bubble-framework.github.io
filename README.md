360545334663875e7cbfd53226863b3c41192377
Missy Lovegren <audrey.lovegren@gmail.com>
2022-08-10 20:29:32 -0500

    Clone Synapse case study, update UI and add sample content

---
+++ README.md
@@ -0,0 +1,1 @@
+# jjam-bubble.github.io
---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const words = ["deploys", "manages", "destroys"];
+let i = 0;
+
+const _changeText = () => {
+  i = (i + 1) % words.length;
+  document.getElementById("spin").innerHTML = `${words[i]}&nbsp;`;
+};
+
+setInterval("_changeText()", 800);
